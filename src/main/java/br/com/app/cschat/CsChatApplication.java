e7b64c6cf45c6adb4192bb5df3683637a0cb5130
Andre Marques <andremarques2911@gmail.com>
2021-05-10 19:30:15 -0300

    Start spring project

---
+++ src/main/java/br/com/app/cschat/CsChatApplication.java
@@ -0,0 +1,13 @@
+package br.com.app.cschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsChatApplication.class, args);
+	}
+
+}
